e431dff7d471a6269047adbae60c3442789c4cc3
flemish4 <willumchandler@gmail.com>
2018-02-26 23:38:44 +0000

    Added BreezySlam code pre-integration

---
+++ simulator/BreezySLAM-master/examples/roboPorterTest.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+# Map size, scale
+MAP_SIZE_PIXELS          = 800
+MAP_SIZE_METERS          =  32
+
+from breezyslam.algorithms import Deterministic_SLAM, RMHC_SLAM
+
+from roboPorterTestFunctions import roboPorterLaser, load_data, RoboPorter
+from progressbar import ProgressBar
+from pgm_utils import pgm_save
+
+from sys import argv, exit, stdout
+from time import time
+
+def main():
+	    
+    # Grab input args
+    dataset = "testLidar"
+    use_odometry  =  True
+    seed =  0
+    
+	# Load the data from the file, ignoring timestamps
+    lidars, velocities = load_data('.', dataset)
+    
+    # Build a robot model if we want odometry
+    robot = RoboPorter() if use_odometry else None
+        
+    # Create a CoreSLAM object with laser params and optional robot object
+    slam = RMHC_SLAM(roboPorterLaser(), MAP_SIZE_PIXELS, MAP_SIZE_METERS, random_seed=seed)# \
+           #if seed \
+           #else Deterministic_SLAM(roboPorterLaser(), MAP_SIZE_PIXELS, MAP_SIZE_METERS)
+           
+    # Report what we're doing
+    nscans = len(lidars)
+    print('Processing %d scans with%s odometry / with%s particle filter...' % \
+        (nscans, \
+         '' if use_odometry else 'out', '' if seed else 'out'))
+    progbar = ProgressBar(0, nscans, 80)
+    
+    # Start with an empty trajectory of positions
+    trajectory = []
+
+    # Start timing
+    start_sec = time()
+    
+    # Loop over scans    
+    for scanno in range(nscans):
+    
+        print(len(lidars[scanno]))
+        # Convert odometry to velocities
+        velocitity = velocities[scanno] #dxyMillimeters, dthetaDegrees, dtSeconds
+                             
+        # Update SLAM with lidar and velocities
+        slam.update(lidars[scanno], velocitity)
+        # Get new position
+        x_mm, y_mm, theta_degrees = slam.getpos()    
+        
+        # Add new position to trajectory
+        trajectory.append((x_mm, y_mm))
+        
+        # Tame impatience
+        progbar.updateAmount(scanno)
+        stdout.write('\r%s' % str(progbar))
+        stdout.flush()
+
+    # Report elapsed time
+    elapsed_sec = time() - start_sec
+    print('\n%d scans in %f sec = %f scans / sec' % (nscans, elapsed_sec, nscans/elapsed_sec))
+                    
+                                
+    # Create a byte array to receive the computed maps
+    mapbytes = bytearray(MAP_SIZE_PIXELS * MAP_SIZE_PIXELS)
+    
+    # Get final map    
+    slam.getmap(mapbytes)
+    
+    # Put trajectory into map as black pixels
+    for coords in trajectory:
+                
+        x_mm, y_mm = coords
+                               
+        x_pix = mm2pix(x_mm)
+        y_pix = mm2pix(y_mm)
+                                                                                              
+        mapbytes[y_pix * MAP_SIZE_PIXELS + x_pix] = 0;
+                    
+    # Save map and trajectory as PGM file    
+    pgm_save('%s.pgm' % dataset, mapbytes, (MAP_SIZE_PIXELS, MAP_SIZE_PIXELS))
+            
+# Helpers ---------------------------------------------------------        
+
+def mm2pix(mm):
+        
+    return int(mm / (MAP_SIZE_METERS * 1000. / MAP_SIZE_PIXELS))  
+    
+                    
+main()
---
+++ simulator/BreezySLAM-master/examples/roboPorterTestFunctions.py
@@ -0,0 +1,90 @@
+
+from breezyslam.vehicles import WheeledVehicle
+from breezyslam.sensors import Laser
+
+import math
+
+
+# Method to load all from file ------------------------------------------------
+# Each line in the file has the format:
+#
+#  TIMESTAMP  ... Q1  Q1 ... Distances
+#  (usec)                    (mm)
+#  0          ... 2   3  ... 24 ... 
+#  
+#where Q1, Q2 are odometry values
+
+def load_data(datadir, dataset):
+    
+    filename = '%s/%s.dat' % (datadir, dataset)
+    print('Loading data from %s...' % filename)
+    
+    fd = open(filename, 'rt')
+    
+    scans = []
+    velocities = []
+    
+    while True:  
+        
+        s = fd.readline()
+        
+        if len(s) == 0:
+            break       
+            
+        toks = s.split()[3:-1] # ignore ''
+        velocityT = s.split()[3:-1] # ignore ''
+        
+        lidar = [int(tok) for tok in toks]
+        velocity = [float(tok) for tok in velocityT]
+
+        scans.append(lidar)
+        velocities.append(velocity)
+        
+    fd.close()
+        
+    return  scans, velocities
+
+class roboPorterLaser(Laser):
+    
+    def __init__(self):
+                        #scan_size, scan_rate_hz, detection_angle_degrees, distance_no_detection_mm, detection_margin=0, offset_mm=0
+        Laser.__init__(self, 275, 2, 360, 10000, 0, 0)
+        
+# Class for MinesRover custom robot ------------------------------------------
+
+class RoboPorter(WheeledVehicle):
+    
+    def __init__(self):
+        
+        WheeledVehicle.__init__(self, 100, 500)
+        
+        self.ticks_per_cycle = 2000
+                        
+    def __str__(self):
+        
+        return '<%s ticks_per_cycle=%d>' % (WheeledVehicle.__str__(self), self.ticks_per_cycle)
+        
+    def computeVelocities(self, odometry):
+        
+        return WheeledVehicle.computeVelocities(self, odometry[0], odometry[1], odometry[2])
+
+    def extractOdometry(self, timestamp, leftWheel, rightWheel):
+                
+        # Convert microseconds to seconds, ticks to angles        
+        return timestamp / 1e6, \
+               self._ticks_to_degrees(leftWheel), \
+               self._ticks_to_degrees(rightWheel)
+               
+    def odometryStr(self, odometry):
+        
+        return '<timestamp=%d usec leftWheelTicks=%d rightWheelTicks=%d>' % \
+               (odometry[0], odometry[1], odometry[2])
+               
+    def _ticks_to_degrees(self, ticks):
+        
+        return ticks * (180. / self.ticks_per_cycle)
+        
+
+        
+        
+        
